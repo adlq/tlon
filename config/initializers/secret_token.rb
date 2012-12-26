13aa3adc7d22fb2c04c36177746da4c970a08ddd
adlq <dtnghia90@gmail.com>
2012-12-26 22:00:32 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tlon::Application.config.secret_token = '0fa0d49f773823226cb549e8ddd112402e0b765a395fd5b03610282cdbe6d8173d529383e95eaae8aa8619dcb777d173166215470126145a6c21e0cbb1c398e1'
